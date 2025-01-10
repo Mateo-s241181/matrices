6cb23bf6b05dbe2baa2b46d17e657d860153253a
Mateo <mateobarac0712@gmx.de>
2025-01-10 13:48:18 +0100

    asdasd

--- arraytools/arraytools.go
+++ arraytools/arraytools.go
@@ -3,11 +3,27 @@ package arraytools
 // Mult erwartet ein Array und einen skalaren Faktor.
 // Multipliziert jedes Element des Arrays mit dem Faktor.
 func ScalarMult(a []float64, factor float64) {
-	// TODOabx
+
+	//Durch alle Elemente aus a durchrangen
+	for i := range a {
+
+		//Ausgewähltes Element mit faktor multiplizieren
+		//el ist nur eine Kopie vom
+		a[i] *= factor
+	}
+
 }
 
 // Add erwartet zwei Arrays der gleichen Länge.
 // Addiert die Elemente der Arrays paarweise.
+// Ergebnis wird im Array a gespeichert
 func Add(a, b []float64) {
-	// TODO
+
+	if len(a) != len(b) {
+		panic("Die beiden Slices haben nicht die gleiche Länge!")
+	}
+
+	for i := range a {
+		a[i] += b[i]
+	}
 }
--- matrixtools/matrixtools.go
+++ matrixtools/matrixtools.go
@@ -1,34 +1,50 @@
 package matrixtools
 
+import "matrices/arraytools"
+
 // GetRow liefert die i-te Zeile der Matrix m.
 func GetRow(m [][]float64, i int) []float64 {
-	// TODO
-	return []float64{}
+	return m[i]
 }
 
 // GetCol liefert die j-te Spalte der Matrix m.
 func GetCol(m [][]float64, j int) []float64 {
-	col := make([]float64, len(m))
-	// TODO
-	return col
+	return Transposed(m)[j]
 }
 
 // AddRows erwartet eine Matrix und zwei Zeilennummern.
 // Addiert die beiden Zeilen paarweise und speichert das Ergebnis in der ersten Zeile.
 func AddRows(m [][]float64, i, j int) {
-	// TODO
+	arraytools.Add(m[i], m[j])
 }
 
 // ScalarMultRow erwartet eine Matrix, eine Zeilennummer und einen skalaren Faktor.
 // Multipliziert die Zeile mit dem Faktor und speichert das Ergebnis in der Zeile.
 func ScalarMultRow(m [][]float64, i int, factor float64) {
-	// TODO
+	arraytools.ScalarMult(m[i], factor)
 }
 
 // Transpose erwartet eine Matrix und liefert ihre Transponierte.
 // D.h. alle Zeilen der ersten Matrix werden zu Spalten der Transponierten und umgekehrt.
 func Transposed(m [][]float64) [][]float64 {
+
+	// Transponierte Matrix wird initialisiert, die Länge ist die Länge der ersten Reihe
 	transposed := make([][]float64, len(m[0]))
-	// TODO
+
+	//ranged durch die Reihen
+	for r := range transposed {
+
+		//Spaltenanzahl von Transposed initialisieren
+		transposed[r] = make([]float64, len(m))
+
+		//ranged durch die Elemente einer Bestimmten Zeile von Transposed
+		for s := range transposed[r] {
+
+			//Element in Reihe s und Spalte r in m wird in die Transponierte Matrix geschrieben
+			//Reihenwert wird mit Spaltenwert vertauscht
+			transposed[r][s] = m[s][r]
+		}
+	}
+
 	return transposed
 }
