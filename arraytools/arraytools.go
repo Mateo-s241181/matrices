64c9fc4c5df1a44332fdace15f0d93a2144e6460
Mateo <mateobarac0712@gmx.de>
2025-01-10 10:12:53 +0100

    test

--- arraytools/arraytools.go
+++ arraytools/arraytools.go
@@ -3,7 +3,7 @@ package arraytools
 // Mult erwartet ein Array und einen skalaren Faktor.
 // Multipliziert jedes Element des Arrays mit dem Faktor.
 func ScalarMult(a []float64, factor float64) {
-	// TODO
+	// TODOabx
 }
 
 // Add erwartet zwei Arrays der gleichen Länge.
